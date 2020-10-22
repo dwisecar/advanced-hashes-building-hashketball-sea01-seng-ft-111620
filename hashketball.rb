18d626093ec037067819ac863c109dda4e4a11e6
David Wisecarver <dwisecar@gmail.com>
2020-10-22 18:17:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,21 @@
 # Write your code here!
+def game_hash
+  the_hashket = {
+    :home => {
+      :team_name => ""
+      :colors => [] #array of strings like "Brown","Mustard","Lime"
+      :players => [{
+        :player_name => ""
+        :number => 0
+        :shoe => 0
+        :points => 0
+        :rebounds => 0
+        :assists => 0
+        :steals => 0
+        :blocks => 0
+        :slam_dunks => 0
+      }] 
+    }
+    :away => {}
+  }
+end
